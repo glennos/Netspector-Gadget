d726d6996d57e931fa21ebcf54242d0aed8fd8ea
glennos <Floep256!>
2015-02-15 17:08:28 +0100

    addes initial loop for menu

--- menu.py
+++ menu.py
@@ -2,17 +2,23 @@ __author__ = 'Glenn'
 import curses
 from pip._vendor.distlib.compat import raw_input
 import get_ipadresses
+import sys
 
 c = "+"
 h = "-"
-print(c + h*78 + c)
-print(" "*32 + "1: Toon IP adressen")
-print(" "*32 + "Optie 2")
-print(" "*32 + "Optie 3")
-print(" "*32 + "Optie 4")
-print()
-input = optie_input = raw_input(" "*32 + "Kies een optie: ",)
-print(optie_input)
-print(c + h*78 + c)
-if optie_input == '1':
-    print(get_ipadresses.show_ipadresses())
+
+while True:
+    print(c + h*78 + c)
+    print(" "*32 + "1: Toon IP adressen")
+    print(" "*32 + "Optie 2")
+    print(" "*32 + "Optie 3")
+    print(" "*32 + "Optie 4")
+    print(" "*32 + "0: Afsluiten")
+    print()
+    input = optie_input = raw_input(" "*32 + "Kies een optie: ",)
+    print(optie_input)
+    print(c + h*78 + c)
+    if optie_input == '1':
+        print(get_ipadresses.show_ipadresses())
+    elif optie_input == '0':
+        sys.exit(0)
