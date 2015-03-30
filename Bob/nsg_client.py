ca9b59f22794311411d02f540a5bd49c894aa03f
Bob Korthals <bob.korthals@hva.nl>
2015-03-30 11:56:51 -0700

    Kom steeds een beetje verder :)

--- Bob/nsg_client.py
+++ Bob/nsg_client.py
@@ -4,70 +4,76 @@ __author__ = 'root'
 import socket
 from struct import *
 
-def sniffer(packet):
-    # print(s.recv(1025))
+def sniffer():
     try:
-        # take first 20 characters for the ip header
-        ip_header = packet[0:20]
+        s = socket.socket()
+    except socket.error as msg:
+        print('Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
 
-        # now unpack them :)
-        iph = unpack('!BBHHHBBH4s4s' , ip_header)
+    host = socket.gethostname()
+    port = 12345
 
-        version_ihl = iph[0]
-        # version = version_ihl >> 4
-        ihl = version_ihl & 0xF
+    s.connect((host, port))
 
-        iph_length = ihl * 4
+    while 1:
+        try:
+            packet = s.recv(1025)
+            if packet:
+                # take first 20 characters for the ip header
+                ip_header = packet[0:20]
 
-        # ttl = iph[5]
-        # protocol = iph[6]
-        # s_addr = socket.inet_ntoa(iph[8]);
-        # d_addr = socket.inet_ntoa(iph[9]);
+                # now unpack them :)
+                iph = unpack('!BBHHHBBH4s4s', ip_header)
+
+                version_ihl = iph[0]
+                # version = version_ihl >> 4
+                ihl = version_ihl & 0xF
+
+                iph_length = ihl * 4
+
+                # ttl = iph[5]
+                # protocol = iph[6]
+                # s_addr = socket.inet_ntoa(iph[8]);
+                # d_addr = socket.inet_ntoa(iph[9]);
+
+                tcp_header = packet[iph_length:iph_length+20]
+
+                # now unpack them :)
+                tcph = unpack('!HHLLBBHHH', tcp_header)
+
+                source_port = tcph[0]
+                dest_port = tcph[1]
+                sequence = tcph[2]
+                acknowledgement = tcph[3]
+                doff_reserved = tcph[4]
+                tcph_length = doff_reserved >> 4
 
 
+                print('Source Port \t\t: ' + str(source_port))
+                print('Dest Port \t\t: ' + str(dest_port))
+                print('Sequence Number \t: ' + str(sequence))
+                print('Acknowledgement \t: ' + str(acknowledgement))
+                print('TCP header length \t: ' + str(tcph_length))
 
-        tcp_header = packet[iph_length:iph_length+20]
+                h_size = iph_length + tcph_length * 4
+                data_size = len(packet) - h_size
 
-        # now unpack them :)
-        tcph = unpack('!HHLLBBHHH' , tcp_header)
+                # get data from the packet
+                data = str(packet[h_size:])
 
-        source_port = tcph[0]
-        dest_port = tcph[1]
-        sequence = tcph[2]
-        acknowledgement = tcph[3]
-        doff_reserved = tcph[4]
-        tcph_length = doff_reserved >> 4
+                if data:
+                    print('Data \t\t\t: ' + data)
 
-
-        print('Source Port \t\t: ' + str(source_port))
-        print('Dest Port \t\t: ' + str(dest_port))
-        print('Sequence Number \t: ' + str(sequence))
-        print('Acknowledgement \t: ' + str(acknowledgement))
-        print('TCP header length \t: ' + str(tcph_length))
-
-        h_size = iph_length + tcph_length * 4
-        data_size = len(packet) - h_size
-
-        # get data from the packet
-        data = str(packet[h_size:])
-
-        if data:
-            print(data)
-
-        print("\n")
-    except KeyboardInterrupt:
-        print("End")
-
-s = socket.socket()
-host = socket.gethostname()
-port = 12345
-
-s.connect((host, port))
+                print("\n")
+            del packet
+        except KeyboardInterrupt:
+            break
+        except:
+            print("End")
 
 while True:
     try:
-        sniffer(s.recv(1025))
-    except KeyboardInterrupt:
-        s.close()
-    finally:
-        s.close()
+        sniffer()
+    except socket.error:
+        break
--- Bob/nsg_server.py
+++ Bob/nsg_server.py
@@ -5,35 +5,45 @@ __author__ = 'root'
 import socket, sys
 from struct import *
 
-def sniffer(remote):
-    # create an INET, STREAMing socket
+def sniffer():
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as msg:
         print('Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
         sys.exit()
 
-    while True:
-        packet = s.recvfrom(65565)
+    host = socket.gethostname()
+    port = 12345
+    s.bind((host, port))
 
-        # packet string from tuple
-        packet = packet[0]
-        remote.sendall(packet)
+    s.listen(5)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
-port = 12345
-s.bind((host, port))
+    while 1:
+        try:
+            c, addr = s.accept()
+            print('Got connection from', addr)
 
-s.listen(5)
-while True:
+            # create an INET, STREAMing socket
+            try:
+                so = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+            except socket.error as msg:
+                print('Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+                sys.exit()
+            while 1:
+                packet = so.recvfrom(65565)
+                # packet string from tuple
+                packet = packet[0]
+                if packet:
+                    c.send(packet)
+        except socket.error:
+            break
+        except KeyboardInterrupt:
+            s.close()
+            break
+
+while 1:
     try:
-        c, addr = s.accept()
-        print('Got connection from', addr)
-        while 1:
-            sniffer(c)
-    except KeyboardInterrupt:
-        s.send("End")
-    finally:
-        s.close()
+        sniffer()
+    except socket.error:
+        break
 
